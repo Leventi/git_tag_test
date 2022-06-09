b87240325315e8d4d1fda49e82b04d9f067dcdd8
Zverev.A <zverev.a@baltlease.ru>
2022-06-09 17:27:10 +0300

    test commit

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import subprocess
+# import git
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from pip._internal.vcs import git
+
+# label = subprocess.check_output(["git", "describe"]).strip()
+# print(label)
+
+repo = git.Repo(search_parent_directories=True)
+sha = repo.head.object.hexsha
+
+print(repo)
+print(sha)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+__version__ = '0.0.2'
